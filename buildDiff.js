9e20b44981fa1245df5a504ce87194b988955bea
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2024-04-17 13:55:09 +0300

    Add files via upload

--- buildDiff.js
+++ buildDiff.js
@@ -7,25 +7,25 @@ const buildDiff = (obj1, obj2) => {
     const value2 = obj2[key];
 
     if (!Object.hasOwn(obj1, key)) {
-      return { key, secondValue: value2, type: 'added' };
+      return { key, value: value2, type: 'added' };
     }
 
     if (!Object.hasOwn(obj2, key)) {
-      return { key, secondValue: value1, type: 'deleted' };
+      return { key, value: value1, type: 'deleted' };
     }
 
-    if (_.isEqual(value1, value2)) {
-      return { key, secondValue: value1, type: 'unchanged' };
+    if (value1 === value2) {
+      return { key, value: value1, type: 'unchanged' };
     }
 
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { key, secondValue: buildDiff(value1, value2), type: 'hasChild' };
+    if (typeof value1 === 'object' && typeof value2 === 'object') {
+      return { key, value: buildDiff(value1, value2), type: 'hasChild' };
     }
     
     return {
       key,
-      firstValue: value1,
-      secondValue: value2,
+      oldValue: value1,
+      value: value2,
       type: 'changed',
     };
   });
