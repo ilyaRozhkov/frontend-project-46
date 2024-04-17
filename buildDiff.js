b1277d115687c95e9706f332b7370edfc5b3f73f
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2024-04-17 13:37:55 +0300

    Add files via upload

--- buildDiff.js
+++ buildDiff.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-
 const buildDiff = (obj1, obj2) => {
   const sortedUnicKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
   const resultObj = sortedUnicKeys.map((key) => {
@@ -7,26 +6,28 @@ const buildDiff = (obj1, obj2) => {
     const value2 = obj2[key];
 
     if (!Object.hasOwn(obj1, key)) {
-      return { key, value: value2, type: 'added' };
+      return { key, secondValue: value2, type: 'added' };
     }
 
     if (!Object.hasOwn(obj2, key)) {
-      return { key, value: value1, type: 'deleted' };
+      return { key, secondValue: value1, type: 'deleted' };
     }
 
     if (_.isEqual(value1, value2)) {
-      return { key, value: value1, type: 'unchanged' };
+      return { key, secondValue: value1, type: 'unchanged' };
     }
 
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { key, value: buildDiff(value1, value2), type: 'hasChild' };
+      return { key, secondValue: buildDiff(value1, value2), type: 'hasChild' };
     }
-    return {
+    const newObj = {
       key,
-      oldValue: value1,
-      value: value2,
+      firstValue: value1,
+      secondValue: value2,
       type: 'changed',
-    };
+
+    }
+    return newObj;
   });
   return resultObj;
 };
