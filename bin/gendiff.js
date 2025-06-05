2ac3019f7717fcc53af1ddba36352bbcd38c320a
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2025-06-05 17:48:51 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,15 +10,13 @@ program
   .version('1.0.0')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, options) => {//Проводимые операции лучше обернуть в try catch
+  .action((filepath1, filepath2, options) => {
     try {
       const diff = genDiff(filepath1, filepath2, options.format);
       console.log(diff);
     } catch (error) {
       console.error('Error:', error.message);
-      process.exit(1);//Так как приложение может упасть, а у нас работает нода, её лучше наглядно вырубать
     }
   });
 
-program.parse(process.argv); //process.argv это массив, который содержит аргументы, передаваемые в командную строку при запуске скрипта
-// поэтому стоит его наглядно передавать
+program.parse(process.argv); 
