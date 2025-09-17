2c101d486560323ecfb4716dac6d5d8aa16b81de
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2025-09-17 18:45:55 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,7 @@
 #!/usr/bin/env node
+import { program } from 'commander'
+import genDiff from '../src/index.js'
 
-    const commander = require('commander');
-
-
-// https://ru.hexlet.io/blog/posts/skripty-moduli-i-biblioteki
-// Импорт ровно одной функции правильной сигнатуры. Все остальное – кишки библиотеки
-import genDiff from '../index.js'
-
-    commander.program
-      .version('0.0.1')
-  .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish') // Дефолтный формат – 4 параметр
-  .action((path1, path2) => { // Код вызова внутри action
-    // Вывод на экран происходит здесь, а не внутри библиотеки
+program.version('0.0.1').arguments('<filepath1> <filepath2>').description('Compares two configuration files and shows a difference').option('-f, --format [type]', 'output format (default: "stylish")', 'stylish').action((path1, path2) => {
     console.log(genDiff(path1, path2, program.opts().format))
-  })
-  .parse(process.argv)
-
+  }).parse(process.argv)
